ab0972eb2ba8e4e2034ab0fc9b1c6f8141c4c319
Mahdi (Matthew) <55137145+mhdizmni@users.noreply.github.com>
2026-04-17 20:18:37 +0330

    Delete apps/blog directory

--- apps/blog/src/lib/search-types.ts
+++
@@ -1,9 +0,0 @@
-export type BlogSearchResult = {
-    id: string;
-    type: "page";
-    content: string;
-    url: string;
-    description: string;
-    heroImagePath: string;
-    tags: string[];
-  };
